1dd68bbad73445cb4b57e5bb94ce683432f759e7
Roman <rombom24@gmail.com>
2022-08-04 13:17:36 +0300

    add left and right anim

--- js/script.js
+++ js/script.js
@@ -17,18 +17,27 @@ class Slider{
 		this.images = this.root.querySelectorAll('.photos img');
 		this.i = 0;
 		this.animated = false;
-		this.vector 
 
 		this.btnPrev.addEventListener('click', () => { this.prev() });
 		this.btnNext.addEventListener('click', () => { this.next() });
+
+		this.leftAnim = [
+			{ transform: 'translateX(-100%)' },
+			{ transform: 'translateX(0)' }
+		];
+
+		this.rightAnim = [
+			{ transform: 'translateX(100%)' },
+			{ transform: 'translateX(0)' }
+		];
+
 	}
 
 	prev(){
 		if(!this.animated){
 			let imgHide = this.images[this.i];
 			this.i = this.i > 0 ? this.i - 1 : this.images.length - 1;
-			this.vector = 'l'
-			this.toogleSlides(imgHide, this.images[this.i]);
+			this.toogleSlides(imgHide, this.images[this.i], isNext = false);
 		}
 	}
 
@@ -36,28 +45,30 @@ class Slider{
 		if(!this.animated){
 			let imgHide = this.images[this.i];
 			this.i = this.i < this.images.length - 1 ? this.i + 1 : 0;
-			this.vector = 'r'
 			this.toogleSlides(imgHide, this.images[this.i]);
-			
 		}
 	}
 
-	toogleSlides(imgHide, showImg){
-		this.animated = true;
-		let animate 
-		let animate2
-		if (this.vector === 'r') { 
-			animate = imgHide.animate([{ transform: 'translateX(100%)' }], 500);
-			// animate2 = showImg.animate([{ transform: 'translateX(0)' }], 500);
-		} else if (this.vector === 'l') { 
-			animate = imgHide.animate([{ transform: 'translateX(-100%)' }], 500);
-		}
+	toogleSlides(imgHide, showImg, isNext = true){
+		// this.animated = true;
+		// let animate 
+		// let animate2
+		// if (this.vector === 'r') { 
+		// 	animate = imgHide.animate([{ transform: 'translateX(100%)' }], 500);
+		// 	// animate2 = showImg.animate([{ transform: 'translateX(0)' }], 500);
+		// } else if (this.vector === 'l') { 
+		// 	animate = imgHide.animate([{ transform: 'translateX(-100%)' }], 500);
+		// }
 
-		animate.addEventListener('finish', () => {
-			imgHide.classList.remove('showed');
-			this.animated = false;
-		});
+		// animate.addEventListener('finish', () => {
+		// 	imgHide.classList.remove('showed');
+		// 	this.animated = false;
+		// });
 
 		showImg.classList.add('showed');
+		showImg.animate(isNext ? this.rightAnim : this.leftAnim, {duration:500});
+
+
+		imgHide.classList.remove('showed');
 	}
 }
