2ef853fcd6d015dcc44049578a132fabc397249b
Roman <rombom24@gmail.com>
2022-08-04 11:56:33 +0300

    img-hide left and rigth

---
+++ js/script.js
@@ -0,0 +1,63 @@
+window.addEventListener('load', function(){
+
+	new Slider('.gallery-1');
+	let slider2 = new Slider('.gallery-2');
+
+	setInterval(function(){
+		slider2.next();
+	}, 3000);
+});
+
+class Slider{
+	constructor(selector){
+		this.root = document.querySelector(selector);
+		this.btnPrev = this.root.querySelector('.buttons .prev');
+		this.btnNext = this.root.querySelector('.buttons .next');
+	
+		this.images = this.root.querySelectorAll('.photos img');
+		this.i = 0;
+		this.animated = false;
+		this.vector 
+
+		this.btnPrev.addEventListener('click', () => { this.prev() });
+		this.btnNext.addEventListener('click', () => { this.next() });
+	}
+
+	prev(){
+		if(!this.animated){
+			let imgHide = this.images[this.i];
+			this.i = this.i > 0 ? this.i - 1 : this.images.length - 1;
+			this.vector = 'l'
+			this.toogleSlides(imgHide, this.images[this.i]);
+		}
+	}
+
+	next(){
+		if(!this.animated){
+			let imgHide = this.images[this.i];
+			this.i = this.i < this.images.length - 1 ? this.i + 1 : 0;
+			this.vector = 'r'
+			this.toogleSlides(imgHide, this.images[this.i]);
+			
+		}
+	}
+
+	toogleSlides(imgHide, showImg){
+		this.animated = true;
+		let animate 
+		let animate2
+		if (this.vector === 'r') { 
+			animate = imgHide.animate([{ transform: 'translateX(100%)' }], 500);
+			// animate2 = showImg.animate([{ transform: 'translateX(0)' }], 500);
+		} else if (this.vector === 'l') { 
+			animate = imgHide.animate([{ transform: 'translateX(-100%)' }], 500);
+		}
+
+		animate.addEventListener('finish', () => {
+			imgHide.classList.remove('showed');
+			this.animated = false;
+		});
+
+		showImg.classList.add('showed');
+	}
+}
